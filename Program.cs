b3dd382b1e3a36c5f7119c1ba376f1051223d894
Utkarsh299-tech <singhutkarsh2799@gmail.com>
2021-01-11 21:35:35 +0530

    Created Console App

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("It's Local Hack Day: Build Week! Let's Hack!");
+            var name = "Utkarsh";
+            Console.WriteLine($"I am {name}");
+        }
+    }
+}
